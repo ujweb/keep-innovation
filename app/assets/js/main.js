d6a647b555a74739a2ee6bb165b37c36b7a1d66a
Iris Huang <iris@digisalad.cool>
2025-06-04 02:06:20 +0800

    update: relative link and logos

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -34,104 +34,12 @@ $(document).ready(() => {
     });
 });
 
-AOS.init();
-
-const slider__item = document.querySelectorAll(".slider__item")
-const createHeroBannerFlicking = (selector) => {
-    return new Flicking(selector, {
-        circular: true,
-        renderOnlyVisible: true,
-    })
-        .on("willChange", function (e) {
-            const index = e.index;
-            onChangeHeroBannerBackground(index);
-            onChangeHeroBannerFlicking(index);
-            // TODO: 圖片 Blur-In
-            slider__item.forEach(element => {
-                element.style.filter = "blur(8px)"
-            });
-        })
-        .on("changed", function (e) {
-            const index = e.index;
-            // TODO: 建立圖片往上位移滾動機制
-            // TODO: 圖片 Blur-Out
-            slider__item.forEach(element => {
-                element.style.filter = "blur(0)"
-            });
-        });
-}
-
-// 建立四個 Flicking 實例
-const selectors = [
-    "#slider__item__flicking--1",
-    "#slider__item__flicking--2",
-    "#slider__item__flicking--3",
-    "#slider__item__flicking--4"
-];
-
-const heroBannerFlickings = selectors.map(createHeroBannerFlicking);
-
-// 加入 Sync 插件（假設第一個為主控）
-heroBannerFlickings[0].addPlugins(
-    new Flicking.Plugins.Sync({
-        type: "camera",
-        synchronizedFlickingOptions: heroBannerFlickings.map(flicking => ({
-            flicking,
-            isClickable: false
-        }))
-    })
-);
-heroBannerFlickings[0].addPlugins(new Flicking.Plugins.AutoPlay({ duration: 5000 }));
-heroBannerFlickings[0].addPlugins(new Flicking.Plugins.Fade());
-
-const onChangeHeroBannerBackground = (index) => {
-    switch (index) {
-        case 0:
-            switchToColorSet1();
-            break;
-        case 1:
-            switchToColorSet2();
-            break;
-        case 2:
-            switchToColorSet3();
-            break;
-        case 3:
-            switchToColorSet4();
-            break;
-        default:
-            break;
+document.querySelectorAll(".img__container img").forEach((el) => {
+    const container = el.parentElement;
+    const width = el.offsetWidth;
+    if (width < 144) {
+        container.classList.add("img__container--lg");
+    } else {
+        container.classList.remove("img__container--lg");
     }
-};
-
-const pagination = document.querySelectorAll('.hero__banner--pagination button')
-const onChangeHeroBannerFlicking = (index) => {
-    pagination.forEach(element => {
-        element.classList.remove('active')
-    });
-    pagination[index].classList.add('active')
-};
-
-const onChangeHeroFlicking = (index) => {
-    heroBannerFlickings[0].moveTo(index)
-};
-
-const flickingOptions = {
-    circular: true,
-    bound: true,
-    panelsPerView: 1,
-    align: "center",
-    moveType: "strict",
-    renderOnlyVisible: true,
-};
-
-document.querySelectorAll(".item__slider--flicking").forEach((el) => {
-    const flicking = new Flicking(el, flickingOptions);
-
-    const arrowPlugin = new Flicking.Plugins.Arrow({
-        parentEl: el, // 指向每個自己的容器
-        prevElSelector: ".flicking-arrow-prev",
-        nextElSelector: ".flicking-arrow-next",
-    });
-
-    flicking.addPlugins(arrowPlugin);
 });
